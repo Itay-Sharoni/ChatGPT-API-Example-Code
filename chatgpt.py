6a9e8c2f1672c08e620830cd6c4a83460d7001a2
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2023-03-03 22:36:40 +0200

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,51 @@
+import sys
+import openai
+
+
+# Set up your OpenAI API key
+openai.api_key = "<API_KEY>"
+bot_bahviour = "You are a little princess that answer any question"
+#bot_bahviour = "You are a angry bot that comlplain about everything"
+
+# Define a function to generate chat responses
+def generate_chat_response(prompt, history):
+    messages = history + [
+        {"role": "system", "content": bot_bahviour},
+        {"role": "user", "content": prompt}
+    ]
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+            )
+    return response['choices'][0]['message']['content']
+
+# Define a function to prompt the user for input and generate a response
+def chat_with_gpt(user_input=None):
+    history = []
+    while True:
+        if user_input:
+            prompt = user_input
+            user_input = None
+        else:
+            prompt = input("\nYou: ")
+        if prompt.lower() == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        chatbot_response = generate_chat_response(prompt, history)
+        history.append({"role": "user", "content": prompt})
+        history.append({"role": "assistant", "content": chatbot_response})
+        print(f"\nChatbot: {chatbot_response}")
+        if user_input is None:
+            continue
+        else:
+            return chatbot_response
+
+if len(sys.argv) == 2:
+    user_input = sys.argv[1]
+    history = []
+    result = generate_chat_response(user_input, history)
+    #result = chat_with_gpt(user_input)
+    print(result)
+    sys.exit()
+else:
+    chat_with_gpt()
